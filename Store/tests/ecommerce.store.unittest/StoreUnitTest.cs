3effda960e182266a0cbb130e6ae789620d65722
nastain <n.nastain.92@gmail.com>
2014-03-06 11:57:01 +0700

    [Unit Test] Store

--- Store/tests/ecommerce.store.unittest/StoreUnitTest.cs
+++ Store/tests/ecommerce.store.unittest/StoreUnitTest.cs
@@ -6,11 +6,18 @@ using Microsoft.Practices.ServiceLocation;
 using ecommerce.store.repository;
 using NUnit.Framework;
 using ecommerce.datamodel;
+using System.Collections.ObjectModel;
 
 namespace ecommerce.store.unittest
 {
     public class StoreUnitTest : RepositoryBaseTest
     {
+        private Store store = null;
+        private IObjectContextManager _objectContext;
+        private IStoreRepository _repository;
+        private Collection<Store> storeList = null;
+
+        #region Dummy Data
         private Store storeStub1 = new Store()
         {
             Code = "SPT",
@@ -22,11 +29,20 @@ namespace ecommerce.store.unittest
             Type = "Outlet"
         };
 
+        private Store storeUpdateStub1 = new Store()
+        {
+            Code = "TNA",
+            CreateBy = "nastain",
+            CreatedDate = DateTime.Now,
+            Id = new Guid("9266e2e6-798d-4910-9fbb-29c4a2a1c4a7"),
+            Name = "Adidas Official Store",
+            Location = "Cihampelas Walk",
+            Type = "Official Store"
+        };
+
+        #endregion
 
         #region Test Initialization
-
-        private IObjectContextManager _objectContext;
-        private IStoreRepository _repository;
 
         [SetUp]
         public void Initialization()
@@ -44,23 +60,73 @@ namespace ecommerce.store.unittest
         {
             _repository.Add(storeStub1);
             _repository.SaveChanges();
+
+            //Check whether on database or not
+            store = _repository.GetById(storeStub1.Id);
+            Assert.IsNotNull(store, "Data Store Baru Berhasil Terbuat");
+
+            //Delete so this method can be used again
+            _repository.Delete(storeStub1);
+            _repository.SaveChanges();
         }
 
         [Test]
-        public void GetByIdStoreTest()
+        public void GetByIdstoreTest()
         {
             Store store = _repository.GetById(storeStub1.Id);
-            Assert.IsNotNull(store);
+            Assert.IsNull(store);
+        }
+      
+        [Test]
+        public void AddstoreTest()
+        {
+            //Add first
+            _repository.Add(storeStub1);
+            _repository.SaveChanges();
+
+            //Check whether on database or not
+            store = _repository.GetById(storeStub1.Id);
+            Assert.IsNotNull(store, "Data Head Office Baru Berhasil Terbuat");
+
+            //Delete so this method can be used again
+            _repository.Delete(storeStub1);
+            _repository.SaveChanges();
+        }
+
+        [Test]
+        public void DeletestoreTest()
+        {
+            //Add first to make data dummy
+            _repository.Add(storeStub1);
+            _repository.SaveChanges();
+
+            //Delete
+            _repository.Delete(storeStub1);
+            _repository.SaveChanges();
+
+            //make sure wheter data is no longer in database
+            store = _repository.GetById(storeStub1.Id);
+            Assert.IsNull(store, "Data Store Terhapus");
+        }
+
+        [Test]
+        public void GetAllstoreTest()
+        {
+            storeList = _repository.GetAll();
+            Assert.IsNotNull(storeList, "Data Store tidak Kosong");
         }
 
 
         [Test]
-        public void KaliStoreTest()
+        [ExpectedException(typeof(System.InvalidOperationException))]
+        public void UpdateNullstoreTest()
         {
-            int c = _repository.Kali(5, 2);
-            Assert.AreEqual(c, 10);
+            _repository.Update(storeUpdateStub1);
+            _repository.SaveChanges();
+            store = _repository.GetById(storeUpdateStub1.Id);
         }
-        #endregion
 
+        
+        #endregion
     }
 }
